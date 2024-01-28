a20b794827b49c81d561a40cc36db142a84cb468
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2024-01-28 15:16:32 +0300

    Connect Mongo database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
