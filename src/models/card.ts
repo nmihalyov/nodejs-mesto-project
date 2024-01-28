a405889b30ddafb8e6b5f29f10c0f26d9597e904
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2024-01-28 15:29:02 +0300

    Add entities models

---
+++ src/models/card.ts
@@ -0,0 +1,37 @@
+import mongoose, { ObjectId } from 'mongoose';
+
+interface ICard {
+  name: string;
+  link: string;
+  owner: ObjectId;
+  likes: ObjectId[];
+  createdAt: Date;
+}
+
+const cardSchema = new mongoose.Schema<ICard>({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model('card', cardSchema);
