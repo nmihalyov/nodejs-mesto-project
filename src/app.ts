a2e4cab62ade8833ce5b7417e4d209bf602ef7aa
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2024-04-10 19:03:47 +0300

    Add CORS settings

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { json, urlencoded } from 'express';
 import helmet from 'helmet';
@@ -16,6 +17,13 @@ dotenv.config({ path: `.env${process.env.NODE_ENV === 'dev' ? '.dev' : ''}` });
 
 const { SERVER_PORT = 3001, DATABASE_URL = '' } = process.env;
 const app = express();
+
+app.use(cors({
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
+}));
 
 app.use(cookieParser());
 app.use(json());
