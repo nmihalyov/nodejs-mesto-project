3ebac8b6f384315865529be5a29a14f31989cf87
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2024-04-12 12:24:29 +0300

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-backedn',
+    script: './dist/app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp .env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build && pm2 restart ecosystem.config.js',
+    },
+  },
+};
